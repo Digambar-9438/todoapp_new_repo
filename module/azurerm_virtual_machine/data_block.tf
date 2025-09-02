3f23b8a768b6061022c0ac65924e70c5bae8ce87
Digambar-9438 <digambar.naya6080@gmail.com>
2025-09-02 16:18:02 +0530

    commented on data block

--- module/azurerm_virtual_machine/data_block.tf
+++ module/azurerm_virtual_machine/data_block.tf
@@ -9,3 +9,8 @@ data "azurerm_subnet" "subnetdata" {
   virtual_network_name =var.virtual_network_name
   resource_group_name  = var.resource_group_name
 }
+
+# This data block retrieves information from the specified resource.
+# Use this data source to reference existing resources and their attributes
+# within your Terraform configuration, enabling dynamic and reusable infrastructure.
+# Replace the arguments with appropriate values for your environment.
