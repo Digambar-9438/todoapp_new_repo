56f37596f83abfdf69f9d8abec24caa51b769a32
Digambar-9438 <digambar.naya6080@gmail.com>
2025-09-02 16:34:05 +0530

    adding nsg code with descirp rule

--- module/azurerm_virtual_machine/data_block.tf
+++ module/azurerm_virtual_machine/data_block.tf
@@ -10,6 +10,13 @@ data "azurerm_subnet" "subnetdata" {
   resource_group_name  = var.resource_group_name
 }
 
+
+
+
+# This data block retrieves information from the specified resource.
+# Use this data source to reference existing resources and their attributes
+# within your Terraform configuration, enabling dynamic and reusable infrastructure.
+# Replace the arguments with appropriate values for your environment.
 # This data block retrieves information from the specified resource.
 # Use this data source to reference existing resources and their attributes
 # within your Terraform configuration, enabling dynamic and reusable infrastructure.
--- module/azurerm_virtual_machine/main.tf
+++ module/azurerm_virtual_machine/main.tf
@@ -1,39 +1,42 @@
+# Creates a Network Security Group (NSG) with rules to control inbound and outbound traffic.
+# The NSG can be associated with subnets or network interfaces to enforce security policies.
+# This resource allows you to define custom security rules, such as allowing or denying specific ports, protocols, or IP ranges.
+# Use this NSG to connect and secure your Azure resources according to your network requirements.
 
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "vm-nsg"
+  location            = var.location
+  resource_group_name = var.resource_group_name
 
-# resource "azurerm_network_security_group" "vm_nsg" {
-#   name                = "vm-nsg"
-#   location            = var.location
-#   resource_group_name = var.resource_group_name
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-#   security_rule {
-#     name                       = "SSH"
-#     priority                   = 1001
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "22"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
 
-#   security_rule {
-#     name                       = "HTTP"
-#     priority                   = 1002
-#     direction                  = "Inbound"
-#     access                     = "Allow"
-#     protocol                   = "Tcp"
-#     source_port_range          = "*"
-#     destination_port_range     = "80"
-#     source_address_prefix      = "*"
-#     destination_address_prefix = "*"
-#   }
-# }
-
-# resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
-#   network_interface_id      = azurerm_network_interface.nic.id
-#   network_security_group_id = azurerm_network_security_group.vm_nsg.id
-# }
+resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
 
 
 
