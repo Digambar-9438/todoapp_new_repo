6971c19bd883518cebe3ecf65566fb45ec779141
Digambar-9438 <digambar.naya6080@gmail.com>
2025-09-02 15:57:02 +0530

    commented on some code like nsg

--- module/azurerm_virtual_machine/main.tf
+++ module/azurerm_virtual_machine/main.tf
@@ -1,6 +1,39 @@
 
 
+# resource "azurerm_network_security_group" "vm_nsg" {
+#   name                = "vm-nsg"
+#   location            = var.location
+#   resource_group_name = var.resource_group_name
 
+#   security_rule {
+#     name                       = "SSH"
+#     priority                   = 1001
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "22"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+
+#   security_rule {
+#     name                       = "HTTP"
+#     priority                   = 1002
+#     direction                  = "Inbound"
+#     access                     = "Allow"
+#     protocol                   = "Tcp"
+#     source_port_range          = "*"
+#     destination_port_range     = "80"
+#     source_address_prefix      = "*"
+#     destination_address_prefix = "*"
+#   }
+# }
+
+# resource "azurerm_network_interface_security_group_association" "nic_nsg_assoc" {
+#   network_interface_id      = azurerm_network_interface.nic.id
+#   network_security_group_id = azurerm_network_security_group.vm_nsg.id
+# }
 
 
 
