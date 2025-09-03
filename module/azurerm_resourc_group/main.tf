8968257fd1c6368390828eb111836e221a1f1681
Digambar-9438 <digambar.naya6080@gmail.com>
2025-09-03 11:59:31 +0530

    add new rg on ci

--- module/azurerm_resourc_group/main.tf
+++ module/azurerm_resourc_group/main.tf
@@ -4,3 +4,11 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+
+
+## as per cr new rg created on ci
+resource "azurerm_resource_group" "rg" {
+  name     = "nayak-rg"
+  location = "centeralindia"
+}
